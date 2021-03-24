9b4789f8168f17e85f7572a4d58e7569a6d8a95a
patrickzhang <zyfwol@hotmail.com>
2021-03-24 22:32:24 +0800

    NC15

---
+++ java/src/main/NC15.java
@@ -0,0 +1,36 @@
+//二叉树的层序遍历
+
+import java.util.*;
+
+public class NC15 {
+    /**
+     *
+     * @param root TreeNode类
+     * @return int整型ArrayList<ArrayList<>>
+     */
+    public ArrayList<ArrayList<Integer>> levelOrder (TreeNode root) {
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            ArrayList<Integer> list=new ArrayList<>();
+            int num=queue.size();
+            for(int i=0;i<num;i++){
+                TreeNode node=queue.poll();
+                list.add(node.val);
+                if(node.left!=null){
+                    queue.offer(node.left);
+                }
+                if(node.right!=null){
+                    queue.offer(node.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+
+}
